0b590db42b76f76d8d7fdb100494c5f25f27a397
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2022-01-14 16:13:20 +0100

    Aggiunta pagina selezione carta

---
+++ template/cards_template.php
@@ -0,0 +1,19 @@
+<?php
+    $client = $db->users()->getClientById($_SESSION['id']);
+    $cards = $db->cards()->getClientCards($client->getId());
+?>
+<h1>Metodo di pagamento</h1>
+<section id="cards">
+    <?php foreach($cards as $card): ?>
+        <input type="radio" id="<?php echo $card->getId() ?>" name="card" value="<?php echo $card->getId() ?>">
+        <label for="<?php echo $card->getId() ?>"><?php echo 
+            "Titolare: ".$card->getClient().", "
+            ."Numero: ".$card->getNumber().", "
+            ."Data di scadenza: ".$card->getExpire_date().", " ?></label>
+    <?php endforeach; ?>
+</section>
+
+<button type="button">Aggiungi carta</button>
+<button type="button">Prosegui</button>
+
+<!-- el: FOOTER -->
