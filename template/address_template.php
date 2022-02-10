4e34d32a3aaa814567a1cf294a97c91848eb8edc
Marco <marco.frattarola@studio.unibo.it>
2022-02-10 22:30:15 +0100

    modifiche generali

--- template/address_template.php
+++ template/address_template.php
@@ -6,14 +6,14 @@
     <form id="addresses" method="POST" action="cards.php">
         <?php foreach($addresses as $address): ?>
             <div class="address">
-                <input type="radio" id="<?php echo $address->getId() ?>" name="address" value="<?php echo $address->getId() ?>" checked>
-                <label for="<?php echo $address->getId() ?>">
+                <div><label for="<?php echo $address->getId() ?>">
                     <h3><?php echo $address->getStreet() ?>, <?php echo $address->getHousenumber() ?></h3>
                     <h4><?php echo $address->getDetails() ?></h4>
-                </label>
+                </label></div>
+                <input type="radio" id="<?php echo $address->getId() ?>" name="address" value="<?php echo $address->getId() ?>" checked/>
             </div>
         <?php endforeach; ?>
 
-        <input type="submit" value="Prosegui">
+        <input type="submit" value="Prosegui"/>
     </form>
 </section>
