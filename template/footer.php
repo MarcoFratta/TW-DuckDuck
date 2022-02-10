5d10fa17c701ad5505618d0c19daeb4dcdb48abf
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2022-02-10 16:04:58 +0100

    Completato footer

--- template/footer.php
+++ template/footer.php
@@ -9,18 +9,40 @@
             <img alt="" src="img/footer-duck.png"/>
         </div>
 
+        
+
         <section>
-            <!-- img socials -->
+            <!-- btns socials -->
+            <img onClick="location.href='http://facebook.com'" alt="facebook" src="img/mix/svg/facebook-icon.svg"/>
+            <img onClick="location.href='http://instagram.com'" alt="instagram" src="img/mix/svg/instagram-icon.svg"/>
+            <img onClick="location.href='http://linkedin.com'" alt="linkedin" src="img/mix/svg/linkedin-icon.svg"/>
+            <img onClick="location.href='http://twitch.tv'" alt="twitch" src="img/mix/svg/twitch-icon.svg"/>
+            <img onClick="location.href='http://twitter.com'" alt="twitter" src="img/mix/svg/twitter-icon.svg"/>
+            <img onClick="location.href='http://youtube.com'" alt="youtube" src="img/mix/svg/youtube-icon.svg"/>
         </section>
+
         <!-- img circuiti pagamento -->
+        <img alt="payments" src="img/payments.png"/>
+
         <!-- img corrieri -->
-        <!-- img circuiti pagamento -->
-        <p>&copy; 2022 Duck Duck Store - All Rights Reserved</p>
+        <img alt="partners" src="img/partners.png"/>
         
         <div>
             <!-- img duck bottom-left -->
             <img alt="" src="img/footer-duck.png"/>
+
+            <!-- first text -->
+            <span>&copy; 2022 Duck Duck Store</span>
+
+            <!-- img green duck center-->
+            <img alt="" src="img/green-duck-head.png"/>
+
+            <!-- second text -->
+            <span>All Rights Reserved</span>
+
             <!-- img duck bottom-right -->
             <img alt="" src="img/footer-duck.png"/>
         </div>
     </footer>
+
+    
