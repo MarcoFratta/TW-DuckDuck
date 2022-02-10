87f41be8529ffeaa0686b2a385588f152923c4a6
Alex Siroli <alex.siroli@studio.unibo.it>
2022-02-10 01:43:08 +0100

    aggiustamenti css

--- new_card.php
+++ new_card.php
@@ -5,6 +5,8 @@
     require_once "bootstrap.php";
 
     $templateParams['title'] = "Nuova carta";
+    $templateParams['header_title'] = "Nuova carta";
+    $templateParams['styles'] = ['<link rel="stylesheet" type="text/css" href="./css/new_card.css?'.time().'" />'];
     require "template/common_top_html.php";
     require "template/header.php";
     $db = DbConnections::mySqlConnection();
