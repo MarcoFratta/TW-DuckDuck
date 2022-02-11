65b7c5bc0a42ef9326a04bb5755fd0b9a8a200be
Marco <marco.frattarola@studio.unibo.it>
2022-02-11 13:21:42 +0100

    correzzione bug carrello

--- add_cart.php
+++ add_cart.php
@@ -3,6 +3,7 @@ require_once "db/connections.php";
 require_once "db/database.php";
 require_once "utils/functions.php";
 include_once "model/product.php";
+require_once "template/common.php";
 
 session_start();
 
@@ -11,7 +12,12 @@ $allowed_parts = ['bottom','base','middle','top'];
 
 if (isset($_POST['type'])) {
     if (userIsLogged() && isSeller()) {
-        die("Non é possibile effettuare acquisti con un account venditore");
+        require "template/common_top_html.php";
+        require "template/header.php";
+        echo displayError("Non é possibile effettuare acquisti con un account venditore");
+        require "template/footer.php";
+        require "template/common_bottom_html.php";
+        return;
     }
     // get the type of the product
     $type = $_POST['type'];
@@ -21,7 +27,11 @@ if (isset($_POST['type'])) {
         $product_id = (int)$_POST['product_id'];
         $product = $db->products()->getNormalProductById($product_id);
         if ($product === false) {
-            die("product does not exists");
+            require "template/common_top_html.php";
+            require "template/header.php";
+            echo displayError("Prodotto non trovato");
+            require "template/footer.php";
+            require "template/common_bottom_html.php";
         }
         $cart_name = "cart_normal";
         $value = $product;
@@ -29,23 +39,39 @@ if (isset($_POST['type'])) {
         $parts = [];
         foreach($allowed_parts as $part){
             if(!isset($_POST[$part.'_id'])){
-                die("errore parametri");
-            } 
+                require "template/common_top_html.php";
+                require "template/header.php";
+                echo displayError("Errore inserimento");
+                require "template/footer.php";
+                require "template/common_bottom_html.php";
+                return;
+            }
+        } 
             array_push($parts,$_POST[$part.'_id']);
-        }
         if (isset($_POST['dimension'])) {
             $price = 0;
             foreach ($parts as $part) {
                 $item = $db->products()->getCustomItemById($part);
                 if ($item == false) {
-                    die("item not existing");
+                    require "template/common_top_html.php";
+                    require "template/header.php";
+                    echo displayError("Prodotto non trovato");
+                    require "template/footer.php";
+                    require "template/common_bottom_html.php";
+                    return;
                 }
-                $price += $item->getPrice();
             }
+                $price += $item->getPrice();
+        }
             $dim_id = $_POST['dimension'];
             $dimension = $db->products()->getDimensionBySize($dim_id);
             if ($dimension == false) {
-                die('dimension not existing');
+                require "template/common_top_html.php";
+                require "template/header.php";
+                echo displayError("Dimensnione non trovata");
+                require "template/footer.php";
+                require "template/common_bottom_html.php";
+                return;
             }
             $price += $dimension->getPrice();
             $product = serialize(new CustomProduct(null, $price, $dim_id, null, $parts));
@@ -53,10 +79,16 @@ if (isset($_POST['type'])) {
             $product_id = $hash;
             $cart_name = "cart_custom";
             $value = $product;
-        } else {
-            die("errore parametri");
         }
+    } else {
+        require "template/common_top_html.php";
+        require "template/header.php";
+        echo displayError("Errore inserimento");
+        require "template/footer.php";
+        require "template/common_bottom_html.php";
+        return;
     }
+    
 
     // Product exists in database, now we can create/update the session variable for the cart
     if (isset($_SESSION[$cart_name]) && is_array($_SESSION[$cart_name])) {
@@ -71,7 +103,7 @@ if (isset($_POST['type'])) {
         // There are no products in cart, this will add the first product to cart
         $_SESSION[$cart_name] = array($product_id => array("quantity" => $quantity, "value" => $value));
     }
-}
+
 // Prevent form resubmission...
 header('location: cart.php');
 exit;
