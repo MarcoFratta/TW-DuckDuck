ffd10e45019ffe7fcb9911c3ba681e85b556b169
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2022-02-12 23:18:52 +0100

    Corretta accessibilità indirizzi

--- template/address_template.php
+++ template/address_template.php
@@ -3,17 +3,19 @@
 ?>
 <section>
     <form id="addresses" method="POST" action="cards.php">
-    <h2>Seleziona il punto di consegna</h2>
-        <?php foreach($addresses as $address): ?>
-            <div class="address">
-                <label for="<?php echo $address->getId() ?>">
-                    <h3><?php echo $address->getStreet() ?>, <?php echo $address->getHousenumber() ?></h3>
-                    <h4><?php echo $address->getDetails() ?></h4>
-                </label>
-                <input type="radio" id="<?php echo $address->getId() ?>" name="address" value="<?php echo $address->getId() ?>"/>
-            </div>
-        <?php endforeach; ?>
-
-        <input type="button" value="Prosegui" onclick="check()"/>
+        <fieldset>
+            <legend>Indirizzi</legend>
+            <h2>Seleziona il punto di consegna</h2>
+                <?php foreach($addresses as $address): ?>
+                    <div class="address">
+                        <label for="<?php echo $address->getId() ?>">
+                            <h3><?php echo $address->getStreet() ?>, <?php echo $address->getHousenumber() ?></h3>
+                            <h4><?php echo $address->getDetails() ?></h4>
+                        </label>
+                        <input type="radio" id="<?php echo $address->getId() ?>" name="address" value="<?php echo $address->getId() ?>"/>
+                    </div>
+                <?php endforeach; ?>
+            <input type="button" value="Prosegui" onclick="check()"/>
+        </fieldset>
     </form>
 </section>
