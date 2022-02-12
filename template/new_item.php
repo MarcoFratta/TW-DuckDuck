0a2aa3d55883f51fffd5805385e09ac0098a90e8
Alex Siroli <alex.siroli@studio.unibo.it>
2022-02-12 18:05:21 +0100

    miglioramenti new item

--- template/new_item.php
+++ template/new_item.php
@@ -8,19 +8,15 @@
     <form method="POST"  enctype="multipart/form-data" action="add_product.php?type=item">
         <div class="box">
             <div class="container">
-                <div>
-                    <div class="container">
-                        <h3>Nome</h3>
-                        <input type="text" name="name">
-                    </div>
-                    <div class="container">
-                        <h3>€</h3>
-                        <input type="number" name="price" step="0.01" min="0">
-                    </div>
-                </div>
+                <h3>Nome</h3>
+                <input type="text" name="name" required>
+            </div>
+            <div class="container">
+                <h3>€</h3>
+                <input type="number" name="price" step="0.01" min="0" required>
             </div>
             <div class="center">
-                <input type="file" name="img">
+                <input type="file" name="img" required>
             </div>
             <div class="container">
                 <label for="layer" id="layer">Strato</label>
